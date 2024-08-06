11d8f4d89b7a26a23958f2b050822e2ce4774dc5
Gal Omer <galomer6708@gmail.com>
2024-08-06 14:02:39 +0300

    first commit

---
+++ Exercise4.py
@@ -0,0 +1,33 @@
+# Exercise 4
+computer_brand = "ASUS"
+print(f" I Have a {computer_brand} computer")
+# Exercise 5
+name = "gal"
+age = 32
+shoe_size = 42
+info = f"Hi my name is {name} i have {age} years of age and my shoe size is {shoe_size} and i am fine with that :-) "
+print(info)
+#  Exercise 6
+a = 10
+b = 5
+if a > b :
+    print("Hello World")
+
+# Exercise 7
+number = int(input("please choose a number from 1 to 100"))
+if number % 2 == 0:
+    print("this number is even ")
+else:
+    print("this number is odd")
+# Exercise 8
+name = input("what is your name?")
+if name == "gal":
+    print("Wow mine is two isn't it great!?")
+else:
+    print("we Don't sure the same name , but i still think U R great")
+#  Exercise 9
+height = int(input("what is your height in centimeters?"))
+if height >= 146:
+    print("You are tall enough to ride")
+else:
+    print("sorry you need to grow some more to ride")
